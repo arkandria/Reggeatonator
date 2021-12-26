f4ab544245823ee5248782705f2612abf987e589
Arkandria <miguelfariasgp@gmail.com>
2021-12-26 15:35:39 -0500

    Added reggeatonator with arrays

---
+++ reggeatonator.js
@@ -0,0 +1,6 @@
+let subj = ['Mami,', 'Baby,', 'Nena,', 'Mamita,', 'Gata,', 'Leona,', 'Loba,'];
+let desire = ['yo deseo', 'necesito', 'tengo que','no aguanto las ganas de', 'me muero por'];
+let hotVerb = ['aprisionarte', 'encenderte', 'sacudirte', 'despeinarte', 'revolcarte', 'hacerte volar'];
+let adverb = ['despacito', 'suave', 'profundo', 'salvaje', 'caliente', 'arriba', 'abajo', 'debajo', 'encima'];
+let timeframe = ['hasta el amanecer', 'toda la noche', 'todo el día', 'un rato', 'toda la canción', 'hasta que cierren la disco', 'hasta que se acabe la fiesta', 'hasta que nos echen del antro'];
+let final = ['sin remordimientos', 'sin pena', 'sin contarle a nadie', 'sin que nadie lo sepa', 'aunque se enteren todos', 'mientras tu novio no está' ];
